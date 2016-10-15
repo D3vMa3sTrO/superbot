3af171503c573476f4d198e5875d52dc23c36ee4
moody <nadoo.ali.15.4@gmail.com>
2016-10-15 20:40:44 +0300

    Update getfile.lua

--- plugins/getfile.lua
+++ plugins/getfile.lua
@@ -4,6 +4,7 @@
      | |/ __| '_ \  / _ \ | |/ /  _|     Dev @h_k_a
      | |\__ \ | | |/ ___ \|   <| |___    Dev @Aram_omar22
      |_||___/_| |_/_/   \_\_|\_\_____|   Dev @IXX_I_XXI
+              CH > @lTSHAKEl_CH
 --]]
 local function run(msg, matches)
   if matches[1] == "جلب ملف" then
@@ -23,3 +24,12 @@ return {
   },
   run = run
 }
+
+--[[ 
+    _____    _        _    _    _____    Dev @lIMyIl 
+   |_   _|__| |__    / \  | | _| ____|   Dev @li_XxX_il
+     | |/ __| '_ \  / _ \ | |/ /  _|     Dev @h_k_a
+     | |\__ \ | | |/ ___ \|   <| |___    Dev @Aram_omar22
+     |_||___/_| |_/_/   \_\_|\_\_____|   Dev @IXX_I_XXI
+              CH > @lTSHAKEl_CH
+--]]
