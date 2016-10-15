f103189a313583a7e13e523bdb8e6e2193dabd1d
moody ali <nadoo.ali.15.4@gmail.com>
2016-10-15 23:06:57 +0300

    Update banbot.lua

--- plugins/banbot.lua
+++ plugins/banbot.lua
@@ -12,7 +12,7 @@ do
 local function run(msg, matches)
 local bot_id = our_id 
 local receiver = get_receiver(msg)
-    if matches[1] == 'طرد البوت' and is_admin1(msg) then
+    if matches[1] == 'زحلك' and is_admin1(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
 	   leave_channel(receiver, ok_cb, false)
     elseif msg.service and msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_admin1(msg) then
@@ -24,7 +24,8 @@ end
  
 return {
   patterns = {
-    "^(طرد البوت)$",
+    "^(زحلك)$",
+    "^[!#/](زحلك)$",
     "^!!tgservice (.+)$",
   },
   run = run
