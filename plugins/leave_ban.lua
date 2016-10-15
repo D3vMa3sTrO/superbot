565d44331116b0644e3cbf7f0a2380cc44793402
moody <nadoo.ali.15.4@gmail.com>
2016-10-15 20:56:51 +0300

    Update leave_ban.lua

--- plugins/leave_ban.lua
+++ plugins/leave_ban.lua
@@ -1,10 +1,11 @@
---[[ 
+—[[ 
     _____    _        _    _    _____    Dev @lIMyIl 
    |_   _|__| |__    / \  | | _| ____|   Dev @li_XxX_il
      | |/ __| '_ \  / _ \ | |/ /  _|     Dev @h_k_a
      | |\__ \ | | |/ ___ \|   <| |___    Dev @Aram_omar22
      |_||___/_| |_/_/   \_\_|\_\_____|   Dev @IXX_I_XXI
---]]
+              CH > @lTSHAKEl_CH
+—]]
 local function run(msg, matches)
 	local data = load_data(_config.moderation.data)
 	if msg.action and msg.action.type then
@@ -31,3 +32,12 @@ return {
   },
   run = run
 }
+
+—[[ 
+    _____    _        _    _    _____    Dev @lIMyIl 
+   |_   _|__| |__    / \  | | _| ____|   Dev @li_XxX_il
+     | |/ __| '_ \  / _ \ | |/ /  _|     Dev @h_k_a
+     | |\__ \ | | |/ ___ \|   <| |___    Dev @Aram_omar22
+     |_||___/_| |_/_/   \_\_|\_\_____|   Dev @IXX_I_XXI
+              CH > @lTSHAKEl_CH
+—]]
