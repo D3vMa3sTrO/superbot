27041025b9c5a4cc6f905f476b496923d57fcd82
moody <nadoo.ali.15.4@gmail.com>
2016-10-15 21:11:57 +0300

    Update msg_checks.lua

--- plugins/msg_checks.lua
+++ plugins/msg_checks.lua
@@ -4,6 +4,7 @@
      | |/ __| '_ \  / _ \ | |/ /  _|     Dev @h_k_a
      | |\__ \ | | |/ ___ \|   <| |___    Dev @Aram_omar22
      |_||___/_| |_/_/   \_\_|\_\_____|   Dev @IXX_I_XXI
+              CH > @lTSHAKEl_CH
 --]]
 local function pre_process(msg)
 -- Begin 'RondoMsgChecks' text checks by @rondoozle
@@ -286,5 +287,11 @@ return {
 	patterns = {},
 	pre_process = pre_process
 }
---End msg_checks.lua
---By @Rondoozle
+--[[ 
+    _____    _        _    _    _____    Dev @lIMyIl 
+   |_   _|__| |__    / \  | | _| ____|   Dev @li_XxX_il
+     | |/ __| '_ \  / _ \ | |/ /  _|     Dev @h_k_a
+     | |\__ \ | | |/ ___ \|   <| |___    Dev @Aram_omar22
+     |_||___/_| |_/_/   \_\_|\_\_____|   Dev @IXX_I_XXI
+              CH > @lTSHAKEl_CH
+--]]
