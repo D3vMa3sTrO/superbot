35fe4b80c6f58c4af3209b35ce5a887df15ef439
moody <nadoo.ali.15.4@gmail.com>
2016-10-15 21:10:02 +0300

    Update lock_tag.lua

--- plugins/lock_tag.lua
+++ plugins/lock_tag.lua
@@ -4,6 +4,7 @@
      | |/ __| '_ \  / _ \ | |/ /  _|     Dev @h_k_a
      | |\__ \ | | |/ ___ \|   <| |___    Dev @Aram_omar22
      |_||___/_| |_/_/   \_\_|\_\_____|   Dev @IXX_I_XXI
+              CH > @lTSHAKEl_CH
 --]]
 local function run(msg, matches)
     if is_momod(msg) then
@@ -32,3 +33,11 @@ return {
 }
 
 
+--[[ 
+    _____    _        _    _    _____    Dev @lIMyIl 
+   |_   _|__| |__    / \  | | _| ____|   Dev @li_XxX_il
+     | |/ __| '_ \  / _ \ | |/ /  _|     Dev @h_k_a
+     | |\__ \ | | |/ ___ \|   <| |___    Dev @Aram_omar22
+     |_||___/_| |_/_/   \_\_|\_\_____|   Dev @IXX_I_XXI
+              CH > @lTSHAKEl_CH
+--]]
