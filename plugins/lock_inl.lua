d7fc58f0a3c8f69ced6812e3325acd5f09ab93ca
moody <nadoo.ali.15.4@gmail.com>
2016-10-15 21:09:38 +0300

    Update lock_inl.lua

--- plugins/lock_inl.lua
+++ plugins/lock_inl.lua
@@ -4,6 +4,7 @@
      | |/ __| '_ \  / _ \ | |/ /  _|     Dev @h_k_a
      | |\__ \ | | |/ ___ \|   <| |___    Dev @Aram_omar22
      |_||___/_| |_/_/   \_\_|\_\_____|   Dev @IXX_I_XXI
+              CH > @lTSHAKEl_CH
 --]]
 do
 local function pre_process(msg)
@@ -38,3 +39,12 @@ run = medo,
 pre_process = pre_process 
 }
 end
+
+--[[ 
+    _____    _        _    _    _____    Dev @lIMyIl 
+   |_   _|__| |__    / \  | | _| ____|   Dev @li_XxX_il
+     | |/ __| '_ \  / _ \ | |/ /  _|     Dev @h_k_a
+     | |\__ \ | | |/ ___ \|   <| |___    Dev @Aram_omar22
+     |_||___/_| |_/_/   \_\_|\_\_____|   Dev @IXX_I_XXI
+              CH > @lTSHAKEl_CH
+--]]
