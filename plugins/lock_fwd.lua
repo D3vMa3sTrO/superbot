4b86dc19d71f48b12a33aa746de7e343fccee7f4
moody ali <nadoo.ali.15.4@gmail.com>
2016-10-17 05:54:13 +0300

    Update lock_fwd.lua

--- plugins/lock_fwd.lua
+++ plugins/lock_fwd.lua
@@ -7,55 +7,45 @@
               CH > @lTSHAKEl_CH
 --]]
 do
+
 local function pre_process(msg)
     
-    --Checking mute
     local hash = 'mate:'..msg.to.id
     if redis:get(hash) and msg.fwd_from and not is_sudo(msg) and not is_owner(msg) and not is_momod(msg)  then
             delete_msg(msg.id, ok_cb, true)
             return "done"
         end
-    
         return msg
     end
+
+  
+
+
+local function moody(msg, matches)
+    chat_id =  msg.to.id
     
- local function mohammed(msg, matches)
-     chat_id = msg.to.id
-local reply_id = msg['id']
-     if is_momod(msg) and matches[1]== 'قفل' and matches[2]== 'اعاده توجيه' then
-         local fwd = 'mate:'..msg.to.id
-         redis:set(fwd, true)
-         local text = ''
-         return reply_msg(reply_id, text, ok_cb, false)
-         end
-local reply_id = msg['id']
-    if not is_momod(msg) and matches[1]== 'قفل' and matches[2]== 'اعاده توجيه' then
-    local text= 'للامنيه فقط #لتلح'
- return reply_msg(reply_id, text, ok_cb, false)
-end
-local reply_id = msg['id']
-if is_momod(msg) and matches[1]== 'فتح' and matches[2]== 'اعاده توجيه' then
-    local fwd = 'mate:'..msg.to.id
-    redis:del(fwd)
-    local text = ''
-    return reply_msg(reply_id, text, ok_cb, false)
+    if is_momod(msg) and matches[1] == 'قفل اعاده توجيه' then
+      
+            
+                    local hash = 'mate:'..msg.to.id
+                    redis:set(hash, true)
+                    return ""
+  elseif is_momod(msg) and matches[1] == 'فتح اعاده توجيه' then
+      local hash = 'mate:'..msg.to.id
+      redis:del(hash)
+      return ""
 end
 
-local reply_id = msg['id']
-if not is_momod(msg) and matches[1]== 'فتح' and matches[2]== 'اعاده توجيه' then
-local text = 'للادمنيه فقط #لتلح'
- return reply_msg(reply_id, text, ok_cb, false)
- end
-
 end
+
 return {
-    patterns ={
-        '^(قفل) (اعاده توجيه)$',
-        '^(فتح) (اعاده توجيه)$',
+    patterns = {
+        '^(قفل اعاده توجيه)$', 
+        '^(فتح اعاده توجيه)$',
         '^[/!#](قفل اعاده توجيه)$', 
         '^[/!#](فتح اعاده توجيه)$' 
     },
-run = mohammed,
+run = moody,
 pre_process = pre_process 
 }
 end
