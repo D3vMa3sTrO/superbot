e00b69d5488cfa8e6cf7966dc8095e80996242ec
moody ali <nadoo.ali.15.4@gmail.com>
2016-10-31 19:49:56 +0000

    Add files via upload

---
+++ plugins/wlc.lua
@@ -0,0 +1,55 @@
+do
+local function run(msg, matches, callback, extra)
+local data = load_data(_config.moderation.data)
+local rules = data[tostring(msg.to.id)]['rules']
+local about = data[tostring(msg.to.id)]['description']
+local hash = 'group:'..msg.to.id
+local group_welcome = redis:hget(hash,'welcome')
+if matches[1] == 'delwlc' and not matches[2] and is_owner(msg) then 
+    
+   redis:hdel(hash,'welcome')
+        return 'تم حذف الترحيب بنجاح✅'
+end
+
+local url , res = http.request('http://api.gpmod.ir/time/')
+if res ~= 200 then return "No connection" end
+local jdat = json:decode(url)
+
+if is_momod(msg) and matches[1] == 'setwlc' then
+redis:hset(hash,'welcome',matches[2])
+        return 'تم حفظ الترحيب💡'
+end
+
+if matches[1] == 'chat_add_user' and msg.service then
+group_welcome = string.gsub(group_welcome, '$userlink', "telegram.me/"..(msg.action.user.username or '').."")
+group_welcome = string.gsub(group_welcome, '$gpname', msg.to.title)
+group_welcome = string.gsub(group_welcome, '$name', ""..(msg.action.user.print_name or '').."")
+  group_welcome = string.gsub(group_welcome, '$username', "@"..(msg.action.user.username or '').."")
+  group_welcome = string.gsub(group_welcome, '$entime', ""..(jdat.ENtime).."")
+  group_welcome = string.gsub(group_welcome, '$endate', ""..(jdat.ENdate).."")
+  group_welcome = string.gsub(group_welcome, '$rules', ""..(rules or '').."")
+  group_welcome = string.gsub(group_welcome, '$about', ""..(about or '').."")
+
+elseif matches[1] == 'chat_add_user_link' and msg.service then
+group_welcome = string.gsub(group_welcome, '$userlink', "telegram.me/"..(msg.from.username or '').."")
+group_welcome = string.gsub(group_welcome, '$gpname', msg.to.title)
+group_welcome = string.gsub(group_welcome, '$name', ""..(msg.from.print_name or '').."")
+  group_welcome = string.gsub(group_welcome, '$username', "@"..(msg.from.username or '').."")
+  group_welcome = string.gsub(group_welcome, '$entime', ""..(jdat.ENtime).."")
+  group_welcome = string.gsub(group_welcome, '$endate', ""..(jdat.ENdate).."")
+  group_welcome = string.gsub(group_welcome, '$rules', ""..(rules or '').."")
+  group_welcome = string.gsub(group_welcome, '$about', ""..(about or '').."")
+
+ end
+return group_welcome
+end
+return {
+  patterns = {
+  "^[!/#](setwlc) +(.*)$",
+  "^[!/#](delwlc)$",
+  "^!!tgservice (chat_add_user)$",
+  "^!!tgservice (chat_add_user_link)$",
+  },
+  run = run
+}
+end
