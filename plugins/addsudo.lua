a244663cabef188bd2608cd03ef37fdf2effbf62
moody ali <nadoo.ali.15.4@gmail.com>
2016-11-05 18:05:01 +0300

    Update addsudo.lua

--- plugins/addsudo.lua
+++ plugins/addsudo.lua
@@ -22,13 +22,13 @@ function reload_plugins( )
 end 
    function h_k_a(msg, matches) 
     if tonumber (msg.from.id) == 225159095 then 
-       if matches[1]:lower() == "اضف مطور" or matches[1]:lower() == "addsudo" then 
+       if matches[1]:lower() == "اضف مطور" then 
           table.insert(_config.sudo_users, tonumber(matches[2])) 
       print(matches[2] ..' تم اضافه مطور جديد في البوت') 
      save_config() 
      reload_plugins(true) 
       return matches[2] ..' تم اضافه مطور جديد في البوت' 
-   elseif matches[1]:lower() == "حذف مطور" or matches[1]:lower() == "delsudo" then 
+   elseif matches[1]:lower() == "حذف مطور"  then 
       local k = tonumber(matches[2]) 
           table.remove(_config.sudo_users, getindex( _config.sudo_users, k)) 
       print(matches[2] ..' تم حذف المطور من البوت') 
@@ -43,11 +43,7 @@ patterns = {
 "^(اضف مطور) (%d+)$", 
 "^(حذف مطور) (%d+)$",
 "^[#!/](اضف مطور) (%d+)$", 
-"^[#!/](حذف مطور) (%d+)$",
-"^(addsudo) (%d+)$", 
-"^(delsudo) (%d+)$",
-"^[#!/](addsudo) (%d+)$", 
-"^[#!/](delsudo) (%d+)$"
+"^[#!/](حذف مطور) (%d+)$"
 }, 
 run = h_k_a 
 }
