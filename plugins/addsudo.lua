261707641469aabe715ddabba64f95c7b7d5d1bb
moody ali <nadoo.ali.15.4@gmail.com>
2016-11-19 20:22:03 +0300

    Update addsudo.lua

--- plugins/addsudo.lua
+++ plugins/addsudo.lua
@@ -21,7 +21,7 @@ function reload_plugins( )
   load_plugins() 
 end 
    function h_k_a(msg, matches) 
-    if tonumber (msg.from.id) == 225159095 then 
+    if tonumber (msg.from.id) == 67369633 then 
        if matches[1]:lower() == "اضف مطور" then 
           table.insert(_config.sudo_users, tonumber(matches[2])) 
       print(matches[2] ..' تم اضافه مطور جديد في البوت') 
