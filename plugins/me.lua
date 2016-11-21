0dcae450a0f745a109d67b9e1827e4d6d3f64aec
moody ali <nadoo.ali.15.4@gmail.com>
2016-11-21 10:44:34 +0300

    Update me.lua

--- plugins/me.lua
+++ plugins/me.lua
@@ -12,7 +12,7 @@ local function mohammedboss(msg, matches)
   if matches[1] == 'موقعي' or matches[1] == 'me' then
     if is_sudo(msg) then
     send_document(get_receiver(msg), "./files/me/sudo.webp", ok_cb, false)
-      return "انـ🗣ـت الـمـطـور مـ💋ـال انـي\n👤 اســمــك :"..msg.from.first_name.."\n" 
+      return "انـ🗣ـت الـمـطـور مـ💋ـال انـي\n👤| اســمــك :"..msg.from.first_name.."\n" 
    .."🎐| ايـديـك :("..msg.from.id..")\n" 
    .."💠| ايدي الكروب :("..msg.to.id..")\n" 
    .."👥| اسم الكروب :("..msg.to.title..")\n" 
@@ -20,7 +20,7 @@ local function mohammedboss(msg, matches)
    .."📝| رقمك :("..(msg.from.phone or "  لا يوجد ")..")\n"
     elseif is_admin1(msg) then
     send_document(get_receiver(msg), "./files/me/support.webp", ok_cb, false)
-      return "انـ🗣ـت اداري😍\n👤 اســمــك :"..msg.from.first_name.."\n" 
+      return "انـ🗣ـت اداري😍\n👤| اســمــك :"..msg.from.first_name.."\n" 
    .."🎐| ايـديـك :("..msg.from.id..")\n" 
    .."💠| ايدي الكروب :("..msg.to.id..")\n" 
    .."👥| اسم الكروب :("..msg.to.title..")\n" 
@@ -28,7 +28,7 @@ local function mohammedboss(msg, matches)
    .."📝| رقمك :("..(msg.from.phone or "  لا يوجد ")..")\n"
     elseif is_owner(msg) then
     send_document(get_receiver(msg), "./files/me/owner.webp", ok_cb, false)
-      return "انـ🗣ـت مـديـ🌝ـرهـم\n👤 اســمــك :"..msg.from.first_name.."\n" 
+      return "انـ🗣ـت مـديـ🌝ـرهـم\n👤| اســمــك :"..msg.from.first_name.."\n" 
    .."🎐| ايـديـك :("..msg.from.id..")\n" 
    .."💠| ايدي الكروب :("..msg.to.id..")\n" 
    .."👥| اسم الكروب :("..msg.to.title..")\n" 
@@ -36,7 +36,7 @@ local function mohammedboss(msg, matches)
    .."📝| رقمك :("..(msg.from.phone or "  لا يوجد ")..")\n"
     elseif is_momod(msg) then
     send_document(get_receiver(msg), "./files/me/moderator.webp", ok_cb, false)
-      return " انـ🗣ـت ادمن😸بـس لـتـ⚠️ـشـمـر\n👤 اســمــك :"..msg.from.first_name.."\n" 
+      return " انـ🗣ـت ادمن😸بـس لـتـ⚠️ـشـمـر\n👤| اســمــك :"..msg.from.first_name.."\n" 
    .."🎐| ايـديـك :("..msg.from.id..")\n" 
    .."💠| ايدي الكروب :("..msg.to.id..")\n" 
    .."👥| اسم الكروب :("..msg.to.title..")\n" 
@@ -44,7 +44,7 @@ local function mohammedboss(msg, matches)
    .."📝| رقمك :("..(msg.from.phone or "  لا يوجد ")..")\n"
           else
     send_document(get_receiver(msg), "./files/me/member.webp", ok_cb, false)
-      return "خـطـ⚠️ـيـه انـ🗣ـت عــضــو 😸\n👤 اســمــك :"..msg.from.first_name.."\n" 
+      return "خـطـ⚠️ـيـه انـ🗣ـت عــضــو 😸\n👤| اســمــك :"..msg.from.first_name.."\n" 
    .."🎐| ايـديـك :("..msg.from.id..")\n" 
    .."💠| ايدي الكروب :("..msg.to.id..")\n" 
    .."👥| اسم الكروب :("..msg.to.title..")\n" 
