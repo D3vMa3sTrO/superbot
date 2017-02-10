0939e431cbfbab1006d5d89b639fc175551a8f83
D3vMa3sTrO <osamaphysical2@gmail.com>
2017-02-10 00:34:59 -0800

    Update kickme-ar.lua

--- plugins/kickme-ar.lua
+++ plugins/kickme-ar.lua
@@ -1,16 +1,16 @@
 --[[ 
-    _____    _        _    _    _____    Dev @lIMyIl 
-   |_   _|__| |__    / \  | | _| ____|   Dev @li_XxX_il
-     | |/ __| '_ \  / _ \ | |/ /  _|     Dev @h_k_a
-     | |\__ \ | | |/ ___ \|   <| |___    Dev @Aram_omar22
-     |_||___/_| |_/_/   \_\_|\_\_____|   Dev @IXX_I_XXI
-              CH > @lTSHAKEl_CH
---]]
+        _    _       _    _____     _____ ____    ____
+       / \  / \     / \  | ____|___|_   _| /_\ \ / __ \     Đєⱴ 💀: @MaEsTrO_0
+      / / \/ / \   / _ \ |  _| / __| | | | |_\_/| |  | |    Đєⱴ 💀: @devmaestr0
+     / / \ \/ \ \ / ___ \| |___\__ \ | | | | \ \| |__| |    Đєⱴ ฿๏ͳ💀: @iqMaestroBot
+    /_/   \/   \_/_/   \_|_____|___/ |_| |_|  \_\\____/     Đєⱴ ฿๏ͳ💀: @maestr0bot
+                   Đєⱴ Ϲḫ₳ͷͷєℓ💀: @DevMaestro
+—]]
 local function run(msg, matches) 
-if matches[1] == 'مغادره' then 
+if matches[1] == 'اطردني' then 
 local hash = 'kick:'..msg.to.id..':'..msg.from.id 
      redis:set(hash, "waite") 
-      return '🎐| يا حبيبي \n🎐| المعرف | @'..msg.from.username..'\n🎐| هل انت متأكد من رغبتك بالمغادره؟\n🎐| ارسل ﴿ نعم ﴾ للتاكيد والمغادره\n🎐| ارسل﴿ لا ﴾ لالغاء المغادره \n🎐| خليك ويانه بتونس 😕🎈' 
+      return '▫️ كبد عمري \n▫️ المعرف | @'..msg.from.username..'\n▫️  هل انت متاكد من رغبتك في المغادره😢\n▫️ ارسل ﴿ نعم ﴾ للتاكيد \n▫️ ارسل﴿ لا ﴾ لالغاء المغادره \n▫️ خليك ويانه ليش تغادر 😕😒' 
     end 
 
     if msg.text then 
@@ -18,7 +18,7 @@ local hash = 'kick:'..msg.to.id..':'..msg.from.id
       if msg.text:match("^نعم$") and redis:get(hash) == "waite" then 
      redis:set(hash, "ok") 
    elseif msg.text:match("^لا$") and redis:get(hash) == "waite" then 
-   send_large_msg(get_receiver(msg), "زين سويت خليك ويانه بتونس 🌚❤️🙊") 
+   send_large_msg(get_receiver(msg), "زين سويت خليك ويانه ليش ماتدخل للقناة اسويك مطور @DevMaestro 🌚🙊") 
      redis:del(hash, true) 
 
       end 
@@ -27,27 +27,64 @@ local hash = 'kick:'..msg.to.id..':'..msg.from.id
     if redis:get(hash) then 
         if redis:get(hash) == "ok" then 
          channel_kick("channel#id"..msg.to.id, "user#id"..msg.from.id, ok_cb, false) 
-         return 'اوك رح اطردك وبعد لترجع ياخراا 😡😐👍🏿 ('..msg.to.title..')' 
+         return 'تم طردك من المجموعه وبعد لا ترجع 😒👍🏿 ('..msg.to.title..')' 
         end 
       end 
     end 
 
 return { 
   patterns = { 
-  '^(مغادره)$', 
+  '^(اطردني)$', 
   '^(نعم)$', 
   '^(لا)$' ,
-  '^[#!/](مغادره)$', 
+  '^[#!/](اطردني)$', 
   '^[#!/](نعم)$', 
   '^[#!/](لا)$'
   }, 
   run = run, 
 }
 --[[ 
-    _____    _        _    _    _____    Dev @lIMyIl 
-   |_   _|__| |__    / \  | | _| ____|   Dev @li_XxX_il
-     | |/ __| '_ \  / _ \ | |/ /  _|     Dev @h_k_a
-     | |\__ \ | | |/ ___ \|   <| |___    Dev @Aram_omar22
-     |_||___/_| |_/_/   \_\_|\_\_____|   Dev @IXX_I_XXI
-              CH > @lTSHAKEl_CH
---]]
+        _    _       _    _____     _____ ____    ____
+       / \  / \     / \  | ____|___|_   _| /_\ \ / __ \     Đєⱴ 💀: @MaEsTrO_0
+      / / \/ / \   / _ \ |  _| / __| | | | |_\_/| |  | |    Đєⱴ 💀: @devmaestr0
+     / / \ \/ \ \ / ___ \| |___\__ \ | | | | \ \| |__| |    Đєⱴ ฿๏ͳ💀: @iqMaestroBot
+    /_/   \/   \_/_/   \_|_____|___/ |_| |_|  \_\\____/     Đєⱴ ฿๏ͳ💀: @maestr0bot
+                   Đєⱴ Ϲḫ₳ͷͷєℓ💀: @DevMaestro
+—]]
+local function run(msg, matches) 
+if matches[1] == 'اطردني' then 
+local hash = 'kick:'..msg.to.id..':'..msg.from.id 
+     redis:set(hash, "waite") 
+      return '▫️ كبد عمري \n▫️ المعرف | @'..msg.from.username..'\n▫️  هل انت متاكد من رغبتك في المغادره😢\n▫️ ارسل ﴿ نعم ﴾ للتاكيد \n▫️ ارسل﴿ لا ﴾ لالغاء المغادره \n▫️ خليك ويانه ليش تغادر 😕😒' 
+    end 
+
+    if msg.text then 
+   local hash = 'kick:'..msg.to.id..':'..msg.from.id 
+      if msg.text:match("^نعم$") and redis:get(hash) == "waite" then 
+     redis:set(hash, "ok") 
+   elseif msg.text:match("^لا$") and redis:get(hash) == "waite" then 
+   send_large_msg(get_receiver(msg), "زين سويت خليك ويانه ليش ماتدخل للقناة اسويك مطور @DevMaestro 🌚🙊") 
+     redis:del(hash, true) 
+
+      end 
+    end 
+   local hash = 'kick:'..msg.to.id..':'..msg.from.id 
+    if redis:get(hash) then 
+        if redis:get(hash) == "ok" then 
+         channel_kick("channel#id"..msg.to.id, "user#id"..msg.from.id, ok_cb, false) 
+         return 'تم طردك من المجموعه وبعد لا ترجع 😒👍🏿 ('..msg.to.title..')' 
+        end 
+      end 
+    end 
+
+return { 
+  patterns = { 
+  '^(اطردني)$', 
+  '^(نعم)$', 
+  '^(لا)$' ,
+  '^[#!/](اطردني)$', 
+  '^[#!/](نعم)$', 
+  '^[#!/](لا)$'
+  }, 
+  run = run, 
+}
